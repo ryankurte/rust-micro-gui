c843254011cbba715f1af6b47ad691555917915e
Ryan Kurte <ryankurte@gmail.com>
2017-06-29 19:39:58 +1200

    Fixed buffer for RGB24 mode

--- src/bin/example.rs
+++ src/bin/example.rs
@@ -1,7 +1,7 @@
 extern crate microgui;
 
 use microgui::types::*;
-use microgui::types::pixel::BW;
+use microgui::types::pixel::*;
 use microgui::buffer::Buffer;
 use microgui::native::Renderer;
 use microgui::graphics::Graphics;
@@ -24,7 +24,12 @@ fn main() {
     let graphics = Graphics::new(0, 0, WIDTH, HEIGHT);
 
     //graphics.draw_line(&mut buffer, point::Point{x: 0, y: 0}, point::Point{x: WIDTH, y: HEIGHT}, &pixel::Pixel::black());
-    graphics.draw_ellipse(&mut buffer, rect::Rect{x: WIDTH/4, y: HEIGHT/4, w: WIDTH/2, h: HEIGHT/2}, &pixel::Pixel::black());
+    //graphics.draw_line(&mut buffer, point::Point{x: 0, y: HEIGHT}, point::Point{x: WIDTH, y: 0}, &pixel::Pixel::black());
+
+    let ellipse_r = WIDTH/4;
+    graphics.draw_ellipse(&mut buffer, rect::Rect{x: (WIDTH-ellipse_r)/3, y: (HEIGHT-ellipse_r)/2, w: ellipse_r, h: ellipse_r}, &pixel::Pixel::red());
+    graphics.draw_ellipse(&mut buffer, rect::Rect{x: (WIDTH-ellipse_r)/2, y: (HEIGHT-ellipse_r)/2, w: ellipse_r, h: ellipse_r}, &pixel::Pixel::green());
+    graphics.draw_ellipse(&mut buffer, rect::Rect{x: (WIDTH-ellipse_r)/3*2, y: (HEIGHT-ellipse_r)/2, w: ellipse_r, h: ellipse_r}, &pixel::Pixel::blue());
 
     // Native renderer allows local display
     let mut renderer = Renderer::new(&"Rust microgui example", WIDTH as u32, HEIGHT as u32);
--- src/buffer.rs
+++ src/buffer.rs
@@ -2,8 +2,9 @@
 //!
 //! Copyright 2017 Ryan Kurte
 
+use std::*;
+
 use types::*;
-use types::buffer::*;
 
 #[derive(Debug)]
 pub struct Buffer<'a> {
@@ -17,14 +18,28 @@ pub struct Buffer<'a> {
 
 /// Base buffer implementation
 /// This implements all supported methods, then maps to types::buffer::{Set, Get} for actual implementations.
+#[allow(dead_code)]
 impl <'a>Buffer<'a> {
-    pub fn new(width: usize, height: usize, porch_bytes: usize, trailer_bytes: usize, data: &mut [u8]) -> Buffer {
+    // Create a new black and white buffer
+    fn new_bw(width: usize, height: usize, porch_bytes: usize, trailer_bytes: usize, data: &mut [u8]) -> Buffer {
         let line_width_bytes = porch_bytes + width / 8 + trailer_bytes;
         return Buffer{width, height, porch_bytes, trailer_bytes, line_width_bytes, data}
     }
 
+    // Create a new rgb24 buffer
+    fn new_rgb24(width: usize, height: usize, porch_bytes: usize, trailer_bytes: usize, data: &mut [u8]) -> Buffer {
+        let line_width_bytes = porch_bytes + width * 3 + trailer_bytes;
+        return Buffer{width, height, porch_bytes, trailer_bytes, line_width_bytes, data}
+    }
+
+    /// Map new to the appropriate buffer type
+    #[cfg(not(any(feature="rgb24")))]
+    pub fn new(width: usize, height: usize, porch_bytes: usize, trailer_bytes: usize, data: &mut [u8]) -> Buffer { Self::new_bw(width, height, porch_bytes, trailer_bytes, data) }
+    #[cfg(feature="rgb24")]
+    pub fn new(width: usize, height: usize, porch_bytes: usize, trailer_bytes: usize, data: &mut [u8]) -> Buffer { Self::new_rgb24(width, height, porch_bytes, trailer_bytes, data) }
+
     /// Black and white mode pixel set function
-    pub fn set_bw(&mut self, x: usize, y: usize, p: &pixel::PixelBW) {
+    fn set_bw(&mut self, x: usize, y: usize, p: &pixel::PixelBW) {
         let index = self.line_width_bytes * y + self.porch_bytes + x / 8;
         let mask = 1 << (7 - x % 8) as u8;
 
@@ -36,8 +51,8 @@ impl <'a>Buffer<'a> {
     }
 
     /// RGB24 mode pixel set function
-    pub fn set_rgb24(&mut self, x: usize, y: usize, p: &pixel::PixelRGB24) {
-        let index: usize = (self.line_width_bytes * y + x) * 3 + self.porch_bytes;
+    fn set_rgb24(&mut self, x: usize, y: usize, p: &pixel::PixelRGB24) {
+        let index: usize = self.line_width_bytes * y + x * 3 + self.porch_bytes;
         
         self.data[index + 0] = p.r;
         self.data[index + 1] = p.g;
@@ -45,7 +60,7 @@ impl <'a>Buffer<'a> {
     }
 
     /// Black and white mode pixel get function
-    pub fn get_bw(&self, x: usize, y: usize) -> pixel::PixelBW {
+    fn get_bw(&self, x: usize, y: usize) -> pixel::PixelBW {
         let index = self.line_width_bytes * y + self.porch_bytes + x / 8;
         let mask = 1 << (7 - x % 8);
 
@@ -57,8 +72,8 @@ impl <'a>Buffer<'a> {
     }  
 
     /// RGB24 mode pixel get function
-    pub fn get_rgb24(&self, x: usize, y: usize) -> pixel::PixelRGB24 {
-        let index: usize = (self.line_width_bytes * y + x) * 3 + self.porch_bytes;
+    fn get_rgb24(&self, x: usize, y: usize) -> pixel::PixelRGB24 {
+        let index: usize = self.line_width_bytes * y + x * 3 + self.porch_bytes;
 
         return pixel::PixelRGB24{r: self.data[index + 0],
                                  g: self.data[index + 1],
@@ -67,7 +82,7 @@ impl <'a>Buffer<'a> {
 }
 
 /// Set pixel implementation for the buffer
-impl <'a>Set for Buffer<'a> {
+impl <'a>buffer::Set for Buffer<'a> {
     /// B/W mode pixel set function
     #[cfg(not(any(feature="rgb24")))]
     fn set(&mut self, x: usize, y: usize, p: &pixel::PixelBW) { self.set_bw(x, y, p) }
@@ -78,7 +93,7 @@ impl <'a>Set for Buffer<'a> {
 }
 
 /// Get pixel implementation for the buffer
-impl <'a>Get for Buffer<'a> {
+impl <'a>buffer::Get for Buffer<'a> {
     /// B/W mode pixel get function
     #[cfg(not(any(feature="rgb24")))]
     fn get(&self, x: usize, y: usize) -> pixel::PixelBW { self.get_bw(x, y) }  
@@ -86,6 +101,21 @@ impl <'a>Get for Buffer<'a> {
     /// RGB mode pixel set function
     #[cfg(feature="rgb24")]
     fn get(&self, x: usize, y: usize) -> pixel::PixelRGB24 { self.get_rgb24(x, y) }
+}
+
+/// Format implementation for the buffer
+impl <'a>fmt::Display for Buffer<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { 
+        write!(f, "[width: {}px height: {}px porch: {}B trailer: {}B line_width: {}B Data:\n", 
+        self.width, self.height, self.porch_bytes, self.trailer_bytes, self.line_width_bytes).unwrap();
+        for l in 0..self.height {
+            let start: usize = l * self.line_width_bytes;
+            let end:   usize = start + self.line_width_bytes - 1;
+            write!(f, "\t\t{:?}\n", &self.data[start..end]).unwrap();
+        }
+        write!(f, "\t]\n").unwrap();
+        Ok(())
+     }
 }
 
 #[cfg(test)]
@@ -105,7 +135,7 @@ mod tests {
     fn test_bw() {
         let mut data: [u8; SIZE_BW] = [0; SIZE_BW];
         let mut data_slice = &mut data[..];
-        let mut buffer = Buffer::new(WIDTH, HEIGHT, PORCH, TRAILER, data_slice);
+        let mut buffer = Buffer::new_bw(WIDTH, HEIGHT, PORCH, TRAILER, data_slice);
 
         buffer.set_bw(0, 0, &true);
         assert_eq!(buffer.get_bw(0, 0), true);
@@ -128,23 +158,33 @@ mod tests {
     fn test_rgb24() {
         let mut data: [u8; SIZE_RGB24] = [0; SIZE_RGB24];
         let mut data_slice = &mut data[..];
-        let mut buffer = Buffer::new(WIDTH, HEIGHT, PORCH, TRAILER, data_slice);
 
-        buffer.set_rgb24(0, 0, &PixelRGB24::black());
-        assert_eq!(buffer.get_rgb24(0, 0), PixelRGB24::black());
-        //assert_eq!(buffer.data[PORCH], 0x80);
+        let blank: [u8; SIZE_RGB24] = [0; SIZE_RGB24];
+        let blank_slice = &blank[..];
+
+        let mut buffer = Buffer::new_rgb24(WIDTH, HEIGHT, PORCH, TRAILER, data_slice);
 
         buffer.set_rgb24(0, 0, &PixelRGB24::white());
         assert_eq!(buffer.get_rgb24(0, 0), PixelRGB24::white());
-        //assert_eq!(buffer.data[PORCH], 0x00);
+        assert_eq!(buffer.data[PORCH + 0], 0xFF);
+        assert_eq!(buffer.data[PORCH + 1], 0xFF);
+        assert_eq!(buffer.data[PORCH + 2], 0xFF);
 
-        buffer.set_rgb24(1, 1, &PixelRGB24::black());
-        assert_eq!(buffer.get_rgb24(1, 1), PixelRGB24::black());
-        //assert_eq!(buffer.data[PORCH + WIDTH / 8 + TRAILER + PORCH], 0x40);
+        buffer.set_rgb24(0, 0, &PixelRGB24::black());
+        assert_eq!(buffer.get_rgb24(0, 0), PixelRGB24::black());
+        assert_eq!(buffer.data, blank_slice);
 
         buffer.set_rgb24(1, 1, &PixelRGB24::white());
         assert_eq!(buffer.get_rgb24(1, 1), PixelRGB24::white());
-        //assert_eq!(buffer.data[PORCH + WIDTH / 8 + TRAILER + PORCH], 0x00);
+
+        let index: usize = (PORCH + WIDTH * 3 + TRAILER) + 3 + PORCH;
+        assert_eq!(buffer.data[index + 0], 0xFF);
+        assert_eq!(buffer.data[index + 1], 0xFF);
+        assert_eq!(buffer.data[index + 2], 0xFF);
+
+        buffer.set_rgb24(1, 1, &PixelRGB24::black());
+        assert_eq!(buffer.get_rgb24(1, 1), PixelRGB24::black());
+        assert_eq!(buffer.data, blank_slice);
     }
 }
 
